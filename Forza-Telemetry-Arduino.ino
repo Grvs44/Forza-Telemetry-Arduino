cbea6d5c153da2efb8a41b22689b8e4bb6afa978
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-08-05 20:54:09 +0100

    Fix RPM LEDs underflow

--- Forza-Telemetry-Arduino.ino
+++ Forza-Telemetry-Arduino.ino
@@ -287,8 +287,13 @@ void printLap(float lap) {
 #ifdef RPM_LEDS
 void updateRpmLeds(Sled* packet) {
   float value = packet->CurrentEngineRpm - packet->EngineIdleRpm;
-  unsigned int increment = (packet->EngineMaxRpm - packet->EngineIdleRpm) / sizeof(rpmLeds);
-  unsigned int ledsOn = ((unsigned int)value) / (increment - 1);
+  unsigned int ledsOn;
+  if (value > 0.0) {
+    unsigned int increment = (packet->EngineMaxRpm - packet->EngineIdleRpm) / sizeof(rpmLeds);
+    ledsOn = ((unsigned int)value) / (increment - 1);
+  } else {
+    ledsOn = 0;
+  }
   unsigned int i = 0;
   while (i <= min(ledsOn, sizeof(rpmLeds) - 1)) {
     digitalWrite(rpmLeds[i++], HIGH);
