78f45f3e3858cd641360f3cc44b16aad7cafab09
Elli Greaves <74414406+Grvs44@users.noreply.github.com>
2025-08-28 15:51:19 +0100

    Update y-threshold

--- settings.h
+++ settings.h
@@ -33,7 +33,7 @@
 // Acceleration thresholds (turn direction LEDs when acceleration larger than this value)
 // {right/left, up/down, forward/backward}
 #define ACC_THRESHOLDS \
-  { 0.5, 0.5, 0.5 }
+  { 0.5, 1.0, 0.5 }
 
 // Include header to optionally override default settings
 #include "_conf.h"
